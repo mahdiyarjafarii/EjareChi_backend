910aeccd13a2921addc72770b928845aa84a937a
Moeinmn <moeinnia@protonmail.com>
2023-06-18 16:03:39 +0330

    feat: database server added to prisma

---
+++ db/deploy.sh
@@ -0,0 +1,8 @@
+dokcer pull postgres
+
+
+docker volume create newVol
+
+docker run --name pg -e POSTGRES_PASSWORD=lol -v /pgdata:/var/lib/postgresql/data -p 5432:5432 -d postgres
+
+docker exec -u postgres -it pg psql
