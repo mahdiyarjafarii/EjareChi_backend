e16544dceb18850e62d0bcef4c5ef3aaee1ee468
Moeinmn <moeinnia@protonmail.com>
2023-06-19 11:08:32 +0330

    versioning added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ProductController } from './v1/product/product.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
+  controllers: [AppController, ProductController],
   providers: [AppService],
 })
 export class AppModule {}
