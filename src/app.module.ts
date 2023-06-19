c8e1710446aada2538ef10e58880aca70158c3ad
Moeinmn <moeinnia@protonmail.com>
2023-06-19 17:08:38 +0330

    prisma added - some routes added for products end point

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { ProductController } from './v1/product/product.controller';
+import { ProductsController } from './v1/products/products.controller';
+import { ProductsModule } from './v1/products/products.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController, ProductController],
+  imports: [ConfigModule.forRoot(), ProductsModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/v1/products/products.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Delete } from '@nestjs/common';
+import { Body, Param } from '@nestjs/common/decorators';
+import { ProductsService } from './products.service';
+import { ProductCreateReq } from './dtos/products.dto';
+
+@Controller({
+  path: 'products',
+  version: '1',
+})
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+  @Get()
+  getProducts(): string {
+    return '';
+  }
+  @Get(':id')
+  getProductByID(@Param('id') id: string): string {
+    return id;
+  }
+  @Post('/create')
+  createProduct(@Body() productDTO: ProductCreateReq): string {
+    return this.productsService.createProductService(productDTO);
+  }
+  @Post('/update/:id')
+  updateProduct(@Param('id') id: string): string {
+    return id;
+  }
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string): string {
+    return id;
+  }
+}
