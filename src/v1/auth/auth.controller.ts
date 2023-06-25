782ee1ec954b86c6a25d5bd0318394f49af7dadd
Moeinmn <moeinnia@protonmail.com>
2023-06-25 11:08:00 +0330

    categories db model added and  module added

--- src/v1/auth/auth.controller.ts
+++ src/v1/auth/auth.controller.ts
@@ -1,33 +1,23 @@
-import { Controller,Get,Post } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserCreateReq } from './dtos/users.dto';
 import { Body, Param } from '@nestjs/common/decorators';
 
-
 @Controller({
-    path: 'auth',
-    version: '1',
-  })
+  path: 'auth',
+  version: '1',
+})
 export class AuthController {
-
-constructor(private readonly authServices:AuthService ){}
-//for login api
-@Get("/login")
-loginUser(){
+  constructor(private readonly authServices: AuthService) {}
+  //for login api
+  @Get('/login')
+  loginUser() {
     return 'test login';
+  }
 
-}
-
-//for signup api
-@Post("signup")
-async signUpUser(
-    @Body() userDTO:UserCreateReq ,
-){
- 
-    return await this.authServices.creatUser(userDTO)
-
-
-}
-
-
+  //for signup api
+  @Post('signup')
+  async signUpUser(@Body() userDTO: UserCreateReq) {
+    return await this.authServices.creatUser(userDTO);
+  }
 }
