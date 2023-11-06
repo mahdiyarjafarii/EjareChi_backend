1e70fb84df613bb7203657159f70add377fbb274
Moein Moeinnia <moeinnia@protonmail.com>
2023-11-06 22:26:30 +0330

    fit: daily rotation logging aded

--- logger/winston.logger.ts
+++ logger/winston.logger.ts
@@ -3,30 +3,38 @@
 
 import { WinstonModule } from 'nest-winston';
 import { transports, format } from 'winston';
+import 'winston-daily-rotate-file'; 
 
 export const instance = WinstonModule.createLogger({
   transports: [
-    // let's log errors into its own file
-    new transports.File({
-      filename: `logs/error.log`,
+    // file on daily rotation (error only)
+    new transports.DailyRotateFile({
+   // %DATE will be replaced by the current date
+      filename: `logs/%DATE%-error.log`, 
       level: 'error',
       format: format.combine(format.timestamp(), format.json()),
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: false, // don't want to zip our logs
+      maxFiles: '30d', // will keep log until they are older than 30 days
     }),
-    // logging all level
-    new transports.File({
-      filename: `logs/combined.log`,
+    // same for all levels
+    new transports.DailyRotateFile({
+      filename: `logs/%DATE%-combined.log`,
       format: format.combine(format.timestamp(), format.json()),
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: false,
+      maxFiles: '30d',
     }),
-    // we also want to see logs in our console
     new transports.Console({
-      format: format.combine(
-        format.cli(),
-        format.splat(),
-        format.timestamp(),
-        format.printf((info) => {
-          return `${info.timestamp} ${info.level}: ${info.message}`;
-        }),
+     format: format.combine(
+       format.cli(),
+       format.splat(),
+       format.timestamp(),
+       format.printf((info) => {
+         return `${info.timestamp} ${info.level}: ${info.message}`;
+       }),
       ),
-    }),
+  }),
   ],
-});
+})
+
